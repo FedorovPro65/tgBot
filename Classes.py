1cd0450df285b0b98289bf7dc5ec2e163b2eef16
667 <pf150765@gmail.com>
2023-11-27 20:36:52 +0300

    Commit add 27_23 Добавил в главное меню кнопки настройки языка меню и ответов - RUS и ENG. Доработал алгоритм, чтобы отслеживать актуальные данные по текущему пользователю, в том числе выбранный язык. Нужно доработать проект в части записи активностей пользователей в лог файл.

--- Classes.py
+++ Classes.py
@@ -1,29 +1,107 @@
 import pandas as pd
 import telebot
-from telebot import types # для указание типов
+from telebot import types  # для указание типов
+from typing import List
+from datetime import datetime
+import time
+
+
+class BotUser:
+    def __init__(self, UserId, UserName):
+        self.UserId = UserId
+        self.UserName = UserName
+        self.DateTimeFirst = datetime.now()
+        self.DateTimeLast = self.DateTimeFirst  # Дата время Последнего входа пользователя
+        self.Language = 'RUS'
+
+    def UpdateDateTimeLast(self):
+        # Присваивает текущее значение дата время для параметра DateTimeLast
+        self.DateTimeLast = datetime.now()
+        print(self.DateTimeLast)
+
+    def UpdateLanguage(self, Language: str):
+        # Присваивает текущее значение дата время для параметра DateTimeLast
+        self.Language = Language
+        print(self.Language)
+
+    def UpdateOLdUser(self, UsersDict:dict, user_id: int):
+
+        self.UserName = UsersDict[user_id].UserName
+        self.DateTimeFirst = UsersDict[user_id].DateTimeFirst
+        self.DateTimeLast = datetime.now()  # Дата время Последнего входа пользователя
+        self.Language = UsersDict[user_id].Language
+
+    def __str__(self) -> str:
+        return (
+            f'Point(id = {self.UserId}, FirstName: {self.UserName},DateTimeFirst: {self.DateTimeFirst.strftime("%H:%M:%S %d.%m.%y")}'
+            f', DateTimeLast: {self.DateTimeLast.strftime("%H:%M:%S %d.%m.%y")}, Language: {self.Language})')
+
+
+def user_add(user: BotUser, Mydict: dict):
+    ''' Добавляем user в словарь пользователей, по любому, если нет строки - создастся, если есть - обновится '''
+    i = user.UserId
+    Mydict[i] = user
+
+def get_user(user_id:int, Mydict: dict, user_name = 'None'):
+    ''' возвращает текущего user по user_id, данные берутся из справочника пользователей если он есть в справочнике,
+    иначе - создается новый пользователь'''
+    if user_id in Mydict:
+        my_user = BotUser(user_id,'OldUserName')
+        my_user.UpdateOLdUser(Mydict, user_id)  # заполняем поля старого из словаря
+    else:
+        my_user = BotUser(user_id, user_name) # создаем нового
+    return my_user
+
+
 class CreateMenu:
-    def __init__(self,filename):
+    def __init__(self, filename):
         '''Конструктор класса. Создает DataFrame с данными для кнопок меню.'''
         # self.__db_path = os.getcwd()
         # self.db_name = os.path.join(self.__db_path, 'database.db')
-        self.filename = filename # 'list_menu.xlsx'
+        self.filename = filename  # 'list_menu.xlsx'
         # Read the values of the file in the dataframe
-        self.data = pd.DataFrame(pd.read_excel(filename), columns=['type_menu', 'order_num', 'btn_name', 'btn_callback'])
+        self.data = pd.DataFrame(pd.read_excel(filename),
+                                 columns=['type_menu', 'order_num', 'type_btn', 'btn_name', 'btn_callback',
+                                          'btn_name_eng', 'btn_callback_eng'])
 
-
-    def select_button(self, type_menu: str) -> dict:
-        '''В качестве аргумента принимает "тип меню". Возвращает словарь где ключ = текст кнопки, значение = callbackdata кнопки'''
+    def select_button(self, type_menu: str, lng='RUS') -> dict:
+        '''В качестве аргумента принимает "тип меню" и язык меню. Возвращает словарь где ключ = текст кнопки, значение = callbackdata кнопки'''
 
         result = dict()
         data = self.data
-        df = data[data['type_menu'] == type_menu][['btn_name', 'btn_callback']].head(13)
+        df = data[data['type_menu'] == type_menu][
+            ['btn_name', 'order_num', 'type_btn', 'btn_callback', 'btn_name_eng', 'btn_callback_eng']].head(13)
         # print('---', type(df), df.count())
         # print(df)
 
         df = df.reset_index()  # make sure indexes pair with number of rows
         for index, row in df.iterrows():
             # print('----', row['btn_name'], '--', row['btn_callback'])
-            result[row['btn_name']] = row['btn_callback']
+            if lng == 'RUS':
+                result[row['btn_name']] = (row['btn_callback'], row['type_btn'])
+            else:
+                result[row['btn_name_eng']] = (row['btn_callback_eng'], row['type_btn'])
         print('Словарь для меню', type_menu, 'создан')
         return result
+
+
+if __name__ == '__main__':
+    # cm = CreateMenu('list_menu.xlsx')
+    # btn_list1 = cm.select_button('Level_1','ENG')
+    # print(btn_list1)
+    d1 = dict()
+    u1 = BotUser(12, 'Ivan')
+    user_add(u1, d1)
+    time.sleep(3)
+    u1.UpdateDateTimeLast()
+    user_add(u1, d1)
+    u1 = BotUser(14, 'Ani')
+    u1.UpdateLanguage('ENG')
+    user_add(u1, d1)
+    for d in d1.keys():
+        print(d1[d])
+    print(d1[14].Language)
+    cur_user = get_user(12,d1)
+    print(cur_user.UserId,cur_user.Language)
+
 
--- defs_tgBot.py
+++ defs_tgBot.py
@@ -3,20 +3,27 @@ from re import X
 # pip install Pillow
 # pip install python-dotenv
 
-from telebot.types import MessageEntity  #
+# from telebot.types import MessageEntity  #
 import os
-import sys
+# import sys
 import telebot
 from telebot import types  # для указание типов
 # import config
-import speech_recognition
-from pydub import AudioSegment
-from PIL import Image, ImageEnhance, ImageFilter
+# import speech_recognition
+# from pydub import AudioSegment
+# from PIL import Image, ImageEnhance, ImageFilter
 from dotenv.main import load_dotenv
-from classes import CreateMenu
+
+import classes
+from classes import CreateMenu, user_add, get_user
 from datetime import datetime, timedelta
 import time
 import locale
+
+
+# Создаем глобальную переменную - множество,  состоящем из словарей вошедших в тг бот пользователей по их id.
+# (id: ( FirstName, DateTimeFirst, DataTimeLast, Language))
+
 
 # Проверяем локальные настройки и часовой пояс, может отличаться на разных серверах, а нам нужно выводить Московское время
 locale.setlocale(locale.LC_ALL, "")
@@ -25,7 +32,7 @@ if time.tzname[0] == 'UTC':
     my_timedelta = 3
 else:
     my_timedelta = 0
-print(lc,time.tzname[0])
+print(lc, time.tzname[0])
 now = datetime.now() + timedelta(hours=my_timedelta)
 dt_in = now.strftime("%H:%M %d.%m.%Y")
 print('===========  тг бот запущен', dt_in, '===========')
@@ -35,93 +42,21 @@ token = os.environ['TOKEN_TGBOT']  # <<< Ваш токен
 # print(token)
 # sys.exit("Exit 47")
 bot = telebot.TeleBot(token)
+# Формируем словари главного и подменю на разных языках.
 cm = CreateMenu('list_menu.xlsx')
-btn_list1 = cm.select_button('main')
-cm_data_dict = cm.select_button('Level_1')
+btn_list1_RUS = cm.select_button('main')
+cm_data_dict_RUS = cm.select_button('Level_1')
+btn_list1_ENG = cm.select_button('main', 'ENG')
+cm_data_dict_ENG = cm.select_button('Level_1', 'ENG')
+
+users_dict = dict()
+
+btn_list1 = btn_list1_RUS
+cm_data_dict = cm_data_dict_RUS
+language = 'RUS'
 
 
 # btn_list = cm.select_button('Level_1')
-
-def transform_image(filename):
-    # Функция обработки изображения
-    source_image = Image.open(filename)
-    enhanced_image = source_image.filter(ImageFilter.EMBOSS)
-    enhanced_image = enhanced_image.convert('RGB')
-    enhanced_image.save(filename)
-    return filename
-
-
-def resize_image(filename, n: int = 2):
-    # Функция уменьшения размера изображения в n раз
-    source_image = Image.open(filename)
-    width = source_image.size[0]
-    height = source_image.size[1]
-    enhanced_image = source_image.resize((width // n, height // n))
-    enhanced_image.save(filename)
-    return filename
-
-
-@bot.message_handler(content_types=['photo'])
-def resend_photo(message):
-    # Функция отправки обработанного изображения
-    file_id = message.photo[-1].file_id
-    x = str(message.caption)
-    try:
-        n = int(x)
-    except:
-        n = 3
-    filename = download_file(bot, file_id)
-
-    # Трансформируем изображение
-    # transform_image(filename)
-    resize_image(filename, n)
-
-    message_caption = 'Картинка уменьшена в ' + str(n) + ' раз.'
-    image = open(filename, 'rb')
-    bot.send_photo(message.chat.id, image, message_caption)
-    image.close()
-
-    # Не забываем удалять ненужные изображения
-    if os.path.exists(filename):
-        os.remove(filename)
-
-
-def oga2wav(filename):
-    # Конвертация формата файлов
-    new_filename = filename.replace('.oga', '.wav')
-    audio = AudioSegment.from_file(filename)
-    audio.export(new_filename, format='wav')
-    return new_filename
-
-
-def recognize_speech(oga_filename):
-    # Перевод голоса в текст + удаление использованных файлов
-    wav_filename = oga2wav(oga_filename)
-    recognizer = speech_recognition.Recognizer()
-
-    with speech_recognition.WavFile(wav_filename) as source:
-        wav_audio = recognizer.record(source)
-
-    text = recognizer.recognize_google(wav_audio, language='ru')
-
-    if os.path.exists(oga_filename):
-        os.remove(oga_filename)
-
-    if os.path.exists(wav_filename):
-        os.remove(wav_filename)
-
-    return text
-
-
-def download_file(bot, file_id):
-    # Скачивание файла, который прислал пользователь
-    file_info = bot.get_file(file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    filename = file_id + file_info.file_path
-    filename = filename.replace('/', '_')
-    with open(filename, 'wb') as f:
-        f.write(downloaded_file)
-    return filename
 
 
 @bot.message_handler(commands=['start'])
@@ -130,9 +65,44 @@ def say_hi(message):
     now = datetime.now() + timedelta(hours=my_timedelta)
     dt_in = now.strftime("%H:%M %d.%m.%Y")
     print(dt_in, ';', message.from_user.id, ';', message.from_user.first_name)
+
+
+    # global btn_list1
+    # global cm_data_dict
+
+    # Обработка поступившего user.id
+    global users_dict
+    cur_user = classes.get_user(message.from_user.id, users_dict, message.from_user.first_name)
+    print('--- say_hi --- ',cur_user)
+    classes.user_add(cur_user, users_dict)
+    print(users_dict)
+    print(users_dict[message.from_user.id])
+    cur_language = cur_user.Language
+    cur_user.UpdateLanguage(cur_language)
+    cur_user.UpdateDateTimeLast()
+    classes.user_add(cur_user, users_dict)
+
+    print(message.from_user.id, users_dict[message.from_user.id].Language)
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # types.InlineKeyboardMarkup()
-    # btn_list = cm.select_button('main')
-    # btn_list1 = btn_list
+
+    if cur_language == 'RUS':
+        btn_list1 = btn_list1_RUS
+        # cm_data_dict = cm_data_dict_RUS
+        text = (
+            " Привет, {0.first_name}! Я бот помощник.\nПомогаю хорошему разработчику, Павлу Федорову, рассказать о себе тем, кому он может быть полезен."
+            "\nНажимайте кнопки для получения информации."
+            "\nНажав кнопку <узнать больше>, Вы попадете в более расширенное меню.").format(
+            message.from_user)
+    else:
+        btn_list1 = btn_list1_ENG
+        # cm_data_dict = cm_data_dict_ENG
+        text = (
+            "Hello, {0.first_name}! I'm an assistant bot.\nI'm helping a good developer, Pavel Fedorov, tell about himself to those who might find him useful."
+            "\nPress buttons for information."
+            "\nClicking the <To learn more> button will take you to a more advanced menu.").format(
+            message.from_user)
+
     list_row_button = []
     for element in btn_list1.items():
         btn = types.KeyboardButton(element[0])
@@ -144,8 +114,7 @@ def say_hi(message):
         row_button = list_row_button[3 * i:3 + (i * 3)]
         markup.row(*row_button)
 
-    text = "Привет, {0.first_name}! Я бот помощник.\n Помогаю хорошему разработчику, Павлу Федорову, рассказать о себе тем, кому он может быть полезен.\n Нажимайте кнопки для получения информации.\n Нажав - узнать больше, Вы попадете в более расширенное меню".format(
-        message.from_user)
+
     bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
@@ -156,9 +125,28 @@ def func(message):
     # print(cm.data)
     # cm_data_dict = cm.select_button('Level_1')
     # print(cm_data_dict)
-    if (message.text == "Назад"):
+    global users_dict
+
+    # global btn_list1
+    # global cm_data_dict
+
+    cur_user = classes.get_user(message.from_user.id, users_dict, message.from_user.first_name)
+    print('--- func 1---', cur_user, cur_user.Language, message.from_user.id)
+    classes.user_add(cur_user, users_dict)
+    cur_language = cur_user.Language
+
+
+    if cur_language == 'RUS':
+        cm_data_dict = cm_data_dict_RUS
+        text1 = "Нажми кнопку, чтобы получить информацию по интересующему вопросу."
+    else:
+        cm_data_dict = cm_data_dict_ENG
+        text1 = "Click the button to receive information on the issue of interest."
+
+    if message.text in ("Назад", "Back"):
         say_hi(message)
-    elif (message.text == "Узнать больше"):
+
+    elif (message.text in ("Узнать больше", "To learn more")):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # types.InlineKeyboardMarkup()
         btn_list = cm_data_dict  # здесь подумать
         # btn_list1 = btn_list
@@ -172,10 +160,26 @@ def func(message):
         for i in range(i_max):
             row_button = list_row_button[3 * i:3 + (i * 3)]
             markup.row(*row_button)
-        bot.send_message(message.chat.id, text="Нажми кнопку, чтобы получить информацию по интересующему вопросу.",
+        bot.send_message(message.chat.id, text=text1,
                          reply_markup=markup)
 
-    elif (message.text == "Фото"):
+    elif (message.text == "RUS"):
+        cur_language = 'RUS'
+        cur_user.UpdateLanguage(cur_language)
+        classes.user_add(cur_user, users_dict)
+        print('--- func 2 ---', cur_user, cur_user.Language, message.from_user.id)
+        cm_data_dict = cm_data_dict_RUS
+        say_hi(message)
+
+    elif (message.text == "ENG"):
+        cur_language = 'ENG'
+        cur_user.UpdateLanguage(cur_language)
+        classes.user_add(cur_user, users_dict)
+        print('--- func 3 ---', cur_user, cur_user.Language, message.from_user.id)
+        cm_data_dict = cm_data_dict_ENG
+        say_hi(message)
+
+    elif (message.text in ("Фото", 'Photo')):
         photos_to_send = open('Fedorov_P_21.jpg', 'rb')
         bot.send_photo(message.chat.id, photos_to_send)
 
@@ -185,11 +189,11 @@ def func(message):
 
     # Доработать , добавив признак обработки (jpg, pdf), чтобы обработка происходила без hard кода.
     # Имя файла и тип обработки читался из словаря на основе настроечного файла XLS.
-    elif (message.text == "Обучение, курсы"):
+    elif (message.text in ("Обучение, курсы", "Training, courses")):
         photos_to_send = open('refresher_courses.jpg', 'rb')
         bot.send_photo(message.chat.id, photos_to_send)
 
-    elif (message.text == "Файл с резюме"):
+    elif (message.text in ("Файл с резюме", "Resume file")):
         doc_to_send = open('Pavel_Fedorov_Staff_eng.pdf', 'rb')
         bot.send_document(chat_id=message.chat.id, document=doc_to_send)
 
@@ -211,4 +215,4 @@ def transcript(message):
 
 # Запускаем бота. Он будет работать до тех пор, пока работает ячейка (крутится значок слева).
 # Для остановки бота надо прервать выполнение программы. Ctrl-C или по другому.
-bot.polling()
+bot.infinity_polling()
